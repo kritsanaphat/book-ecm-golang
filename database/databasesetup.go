904ba6e4a450f81917015ea7644374be17408a48
Kritsanaphat <63010040@kmitl.ac.th>
2022-09-08 22:40:52 +0700

    database

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -1,13 +1,42 @@
 package database
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBSet() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI())
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Panicln("failed to connect mongodb")
+		return nil
+	}
+
+	fmt.Println("sucess to connect to mongobd")
+	return client
 }
+
+var Client *mongo.Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
 
