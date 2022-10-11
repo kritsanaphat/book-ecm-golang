83cdf68f79bb250285c0a814079c1a6008ae3a51
Kritsanaphat <63010040@kmitl.ac.th>
2022-10-11 20:24:07 +0700

    change DB

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -13,8 +13,6 @@ import (
 func DBSet() *mongo.Client {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv:/kritsanaphat:kritsanaphat@librarydb.aik7jkm.mongodb.net/?retryWrites=true&w=majority"))
 
-	"))
-
 	if err != nil {
 		log.Fatal(err)
 	}
